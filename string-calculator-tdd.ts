c20e44ad5ca9b0a1c2489fb44863a553116ccc60
omprakashhivre <83546918+omprakashhivre@users.noreply.github.com>
2025-03-19 23:05:46 +0530

    handled the negative numbers in string

--- string-calculator-tdd.ts
+++ string-calculator-tdd.ts
@@ -1,28 +1,34 @@
 const stringCalculatorTDD = (numbers) => {
-    let actualString = numbers;
-    if(!numbers.trim()) return 0;
-    try{
-        let defaultDRegex = /,|\n/;
-        let customDRegex =  /^\/\/(.+)\n/
+  let actualString = numbers;
+  if(!numbers.trim()) return 0;
+  try{
+      let defaultDRegex = /,|\n/;
+      let customDRegex =  /^\/\/(.+)\n/
 
-        
-        let customDelimiterMatche = numbers.match(customDRegex);
-        if(customDelimiter){
-            delimiter = new RegExp(customDelimiterMatche[1])
-            actualString = numbers.slice(customDelimiterMatch[0].length);
-        }
-        const numberArray = actualString.split(delimiter).map(num => parseInt(num, 10))
-        const sum = numberArray.reduce((sum, number) => sum + number);
-        return sum;
+      
+      let customDelimiterMatche = numbers.match(customDRegex);
+      if(customDelimiterMatche){
+        defaultDRegex = new RegExp(customDelimiterMatche[1])
+          actualString = numbers.slice(customDelimiterMatch[0].length);
+      }
+      const numberArray = actualString.split(defaultDRegex).map(num => parseInt(num, 10))
+
+      const negativeNumbers = numberArray.filter(num => num < 0);
+
+    if (negativeNumbers.length > 0) {
+      throw new Error(`negative numbers not allowed ${negativeNumbers.join(",")}`);
     }
-    catch(e){
-        return e;
-    }
+      const sum = numberArray.reduce((sum, number) => sum + number);
+      return sum;
+  }
+  catch(e){
+      return e.message;
+  }
 }
 
 const sampleInput = ""
 const sampleInput1 = "1"
-const sampleInput2 = "1,5"
+const sampleInput2 = "-1\n-5"
 
 const result  = stringCalculatorTDD(sampleInput2)
 console.log(result)
