fb428bcd8403f0cb8b9b35663f399b198b7d089a
omprakashhivre <83546918+omprakashhivre@users.noreply.github.com>
2025-03-19 23:01:19 +0530

    Added custom delimiter to split the string using it

--- string-calculator-tdd.ts
+++ string-calculator-tdd.ts
@@ -1,7 +1,17 @@
 const stringCalculatorTDD = (numbers) => {
+    let actualString = numbers;
     if(!numbers.trim()) return 0;
     try{
-        const numberArray = numbers.split(/,|\n/).map((number) => parseInt(number))
+        let defaultDRegex = /,|\n/;
+        let customDRegex =  /^\/\/(.+)\n/
+
+        
+        let customDelimiterMatche = numbers.match(customDRegex);
+        if(customDelimiter){
+            delimiter = new RegExp(customDelimiterMatche[1])
+            actualString = numbers.slice(customDelimiterMatch[0].length);
+        }
+        const numberArray = actualString.split(delimiter).map(num => parseInt(num, 10))
         const sum = numberArray.reduce((sum, number) => sum + number);
         return sum;
     }
